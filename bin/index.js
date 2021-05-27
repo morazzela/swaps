b04edebb25f458fb594014a9703995866db7d540
Morazzela <jacquet2.clement@gmail.com>
2021-05-27 15:13:09 +0200

    use json config file

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,18 @@
-const path = require('path');
-const dotenv = require('dotenv');
 const { ethers } = require('ethers');
 const BN = require('decimal.js');
 const axios = require('axios');
+const config = require('../config.json');
 const helpers = require('./helpers');
-const routers = require('./routers');
 const pairAbi = require('../abis/pair.json');
 const tokenAbi = require('../abis/token.json');
 const memes = require('../memes.json');
 
-dotenv.config({
-    path: path.resolve(__dirname, '../.env'),
-});
+const telegramBaseUri = `https://api.telegram.org/bot${config.telegramKey}`;
 
-const telegramBaseUri = `https://api.telegram.org/bot${process.env.TELEGRAM_KEY}`;
-
-const provider = helpers.resolveProvider(process.env.NODE_URL);
-const tokenAddress = ethers.utils.getAddress('0xacd7b3d9c10e97d0efa418903c0c7669e702e4c0');
-// const tokenAddress = ethers.utils.getAddress('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82');
-const usdPair = new ethers.Contract('0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16', pairAbi, provider);
+const { routers } = config;
+const provider = helpers.resolveProvider(config.nodeUrl);
+const tokenAddress = ethers.utils.getAddress(config.tokenAddress);
+const wbnbUsdPair = new ethers.Contract('0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16', pairAbi, provider);
 
 routers.forEach((router) => {
     router.pairs.forEach((pair) => {
@@ -49,7 +43,7 @@ routers.forEach((router) => {
                         address: tokenIn.address,
                         symbol: data[0],
                         amount: new BN(amountIn.toString()).div(new BN(10).pow(data[1])).toFixed(4),
-                        amountNotRounded: new BN(amountIn.toString()).div(new BN(10).pow(data[1])),
+                        amountBN: new BN(amountIn.toString()).div(new BN(10).pow(data[1])),
                     };
 
                     tokenOut = {
@@ -57,7 +51,7 @@ routers.forEach((router) => {
                         address: tokenOut.address,
                         symbol: data[2],
                         amount: new BN(amountOut.toString()).div(new BN(10).pow(data[3])).toFixed(4),
-                        amountNotRounded: new BN(amountOut.toString()).div(new BN(10).pow(data[3])),
+                        amountBN: new BN(amountOut.toString()).div(new BN(10).pow(data[3])),
                     };
 
                     const isBuy = tokenOut.address === tokenAddress;
@@ -76,31 +70,31 @@ routers.forEach((router) => {
                         message += isBuy ? '🟢' : '🔴';
                     }
 
-                    usdPair.getReserves().then((reserves) => {
+                    wbnbUsdPair.getReserves().then((reserves) => {
                         const wbnbPrice = new BN(reserves[1].toString()).div(reserves[0].toString());
 
                         let tokenUsdPrice = 0;
-                        let tokenBnbPrice = 0;
+                        let bnbTokenPrice = 0;
                         message += '<strong>';
                         if (isBuy) {
-                            tokenUsdPrice = tokenIn.amountNotRounded.times(wbnbPrice).div(tokenOut.amountNotRounded).toFixed(3);
-                            tokenBnbPrice = tokenIn.amountNotRounded.div(tokenOut.amountNotRounded).toFixed(6);
+                            tokenUsdPrice = tokenIn.amountBN.times(wbnbPrice).div(tokenOut.amountBN).toFixed(3);
+                            bnbTokenPrice = tokenOut.amountBN.div(tokenIn.amountBN).toFixed(6);
 
                             message += `\n\n1 ${tokenOut.symbol} = $${tokenUsdPrice}\n`;
-                            message += `1 ${tokenOut.symbol} = ${tokenBnbPrice} ${tokenIn.symbol}`;
+                            message += `1 ${tokenIn.symbol} = ${bnbTokenPrice} ${tokenOut.symbol}`;
                         } else {
-                            tokenUsdPrice = tokenOut.amountNotRounded.times(wbnbPrice).div(tokenIn.amountNotRounded).toFixed(3);
-                            tokenBnbPrice = tokenOut.amountNotRounded.div(tokenIn.amountNotRounded).toFixed(6);
+                            tokenUsdPrice = tokenOut.amountBN.times(wbnbPrice).div(tokenIn.amountBN).toFixed(3);
+                            bnbTokenPrice = tokenIn.amountBN.div(tokenOut.amountBN).toFixed(6);
 
                             message += `\n\n1 ${tokenIn.symbol} = $${tokenUsdPrice}\n`;
-                            message += `1 ${tokenIn.symbol} = ${tokenBnbPrice} ${tokenOut.symbol}`;
+                            message += `1 ${tokenOut.symbol} = ${bnbTokenPrice} ${tokenIn.symbol}`;
                         }
 
                         message += `</strong>\n\n<a href="https://bscscan.com/tx/${receipt.transactionHash}">View Transaction</a>`;
 
                         axios.get(`${telegramBaseUri}/sendMessage`, {
                             params: {
-                                chat_id: process.env.TELEGRAM_CHAT_ID,
+                                chat_id: config.telegramChatId,
                                 parse_mode: 'HTML',
                                 disable_web_page_preview: true,
                                 disable_notification: true,
@@ -125,7 +119,7 @@ routers.forEach((router) => {
 
                                 axios.get(`${telegramBaseUri}/sendSticker`, {
                                     params: {
-                                        chat_id: process.env.TELEGRAM_CHAT_ID,
+                                        chat_id: config.telegramChatId,
                                         sticker,
                                         reply_to_message_id: messageId,
                                     },
