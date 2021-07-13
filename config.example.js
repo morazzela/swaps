95dc28c26c4cd13932af7d84f4ff54a89d7c5151
Morazzela <jacquet2.clement@gmail.com>
2021-07-13 21:09:17 +0200

    wip

--- config.example.js
+++ config.example.js
@@ -9,14 +9,28 @@ const config = {
     telegramChatId: '',
     dotCount: 100,
     networks: [{
-        active: false,
+        active: true,
         name: 'BSC',
         url: 'wss://bsc-ws-node.nariox.org:443',
         tokenAddress: addr('0xacd7b3d9c10e97d0efa418903c0c7669e702e4c0'),
         explorerTxUri: (hash) => `https://bscscan.com/tx/${hash}`,
+        weth: addr('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'),
+        usd: addr('0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'),
         routers: [{
             name: 'PancakeSwap V2',
             address: addr('0x10ED43C718714eb63d5aA57B78B54704E256024E'),
+            pairTokens: [
+                addr('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'),
+            ],
+        }, {
+            name: 'PancakeSwap V1',
+            address: addr('0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'),
+            pairTokens: [
+                addr('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'),
+            ],
+        }, {
+            name: 'WaultSwap',
+            address: addr('0xD48745E39BbED146eEC15b79cBF964884F9877c2'),
             pairTokens: [
                 addr('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'),
             ],
@@ -25,8 +39,7 @@ const config = {
         active: true,
         name: 'Polygon',
         url: 'wss://matic-mainnet-archive-ws.bwarelabs.com',
-        // tokenAddress: addr('0xacd7b3d9c10e97d0efa418903c0c7669e702e4c0'),
-        tokenAddress: addr('0x2791bca1f2de4661ed88a30c99a7a9449aa84174'),
+        tokenAddress: addr('0xacd7b3d9c10e97d0efa418903c0c7669e702e4c0'),
         explorerTxUri: (hash) => `https://polygonscan.com/tx/${hash}`,
         weth: addr('0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'),
         usd: addr('0x2791bca1f2de4661ed88a30c99a7a9449aa84174'),
